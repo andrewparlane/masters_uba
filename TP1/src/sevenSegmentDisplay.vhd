c52dd890b4bfaf7308add006f8d48b860e305feb
Andrew Parlane <andrew.parlane@gmail.com>
2018-04-09 12:09:12 -0300

    TP1: sevenSegment: Specifying case values as unsigned hex values.

--- TP1/src/sevenSegmentDisplay.vhd
+++ TP1/src/sevenSegmentDisplay.vhd
@@ -25,18 +25,17 @@ begin
     --      -------
     --         3
 
-    with to_integer(bcd)
-        select sevenSegmentOutput <=
-            (not "0111111") when 0,
-            (not "0000110") when 1,
-            (not "1011011") when 2,
-            (not "1001111") when 3,
-            (not "1100110") when 4,
-            (not "1101101") when 5,
-            (not "1111101") when 6,
-            (not "0000111") when 7,
-            (not "1111111") when 8,
-            (not "1101111") when 9,
+    with bcd select sevenSegmentOutput <=
+            (not "0111111") when 4ux"0",
+            (not "0000110") when 4ux"1",
+            (not "1011011") when 4ux"2",
+            (not "1001111") when 4ux"3",
+            (not "1100110") when 4ux"4",
+            (not "1101101") when 4ux"5",
+            (not "1111101") when 4ux"6",
+            (not "0000111") when 4ux"7",
+            (not "1111111") when 4ux"8",
+            (not "1101111") when 4ux"9",
             (not "0000000") when others;
 
 end architecture synth;
