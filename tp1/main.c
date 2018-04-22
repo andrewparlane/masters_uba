2e04f2e289409aca218758271ef11af04925d608
Andrew Parlane <andrew.parlane@gmail.com>
2018-04-22 15:34:19 -0300

    TP1: Escribí código por leerLinea y escribirSalida.

--- tp1/main.c
+++ tp1/main.c
@@ -4,6 +4,8 @@
 #include <getopt.h>
 #include <ctype.h>
 #include <string.h>
+#include <stdbool.h>
+#include <errno.h>
 
 #define MAJOR_VERSION   0
 #define MINOR_VERSION   1
@@ -42,13 +44,222 @@ static void usage(FILE *stream, const char *nuestroNombre)
     // porque no soportamos %1$s
 }
 
-int leerLinea(long long *data, unsigned int columnasEsperados)
+// lea carácter por carácter deshaciendo whitespace
+// hasta encuentra [0-9-]. Después comenzar a leer números
+// [0-9]. Para cuando obtenemos EOF, \n, \r, ' ', \t.
+// Es un error si encontramos algún otro carácter.
+// devolver no 0 si hay un error
+// *OK = 1 -> hay un integer valido en *ll
+// *eof = 1 -> no hay más a leer
+// *newLine = 1 -> encontramos nueva línea
+bool leerLongLong(FILE *f, long long *ll, bool *OK, bool *eof, bool *newLine)
 {
-#warning TODO
-    return 0;
+    *OK = false;
+    *eof = false;
+    *newLine = false;
+
+    // soportamos signed 64 bits:
+    // máx = 0x7FFF_FFFF_FFFF_FFFF =  9223372036854775807
+    // min = 0x8000_0000_0000_0000 = -9223372036854775808
+    // así max input legal es 20 cáracters +1 por NULL terminator
+#define MAX_CHARS 20
+    char buff[MAX_CHARS + 1];
+    uint idx = 0;
+
+    bool comenzandoLeerInt = false;
+    while (1)
+    {
+        int res = fgetc(f);
+        if (res == EOF)
+        {
+            *eof = true;
+            // si tenemos algo en buff, convertimos ahora
+            if (*OK)
+            {
+                buff[idx] = '\0';
+                *ll = strtoll(buff, NULL, 10);
+                if (errno != 0)
+                {
+                    fprintf(stderr, "Failed to convert %s to long long, error: %s\n", buff, strerror(errno));
+                    return false;
+                }
+                return true;
+            }
+            else if (comenzandoLeerInt)
+            {
+                // solo podríamos estar aquí si leamos
+                // un '-' y después nada, eso es un error
+                fprintf(stderr, "Found invalid entry \"-\"\n");
+                return false;
+            }
+            else
+            {
+                // eof but no error
+                return true;
+            }
+        }
+
+        char c = (char)res;
+        if (c == '\r' || c == '\n')
+        {
+            *newLine = true;
+        }
+
+        if (!comenzandoLeerInt)
+        {
+            // Todavía no cemenzamos a leer el int
+            if (c == ' ' || c == '\t')
+            {
+                // ignoramos
+                continue;
+            }
+            else if (*newLine)
+            {
+                // nuevo línea, terminamos.
+                return true;
+            }
+            else if (c >= '0' && c <= '9')
+            {
+                // válido
+                buff[idx++] = c;
+                comenzandoLeerInt = true;
+                *OK = true;
+            }
+            else if (c == '-')
+            {
+                // también válido pero el int todavía no es OK
+                // porque necesitamos un número después de un -
+                buff[idx++] = c;
+                comenzandoLeerInt = true;
+            }
+            else
+            {
+                // error
+                fprintf(stderr, "Found invalid character %c\n", c);
+                return false;
+            }
+        }
+        else
+        {
+            // ya estamos leyendo data
+            if (c == ' ' || c == '\t' ||
+                *newLine)
+            {
+                // terminamos
+                if (*OK)
+                {
+                    buff[idx] = '\0';
+                    *ll = strtoll(buff, NULL, 10);
+                    if (errno != 0)
+                    {
+                        fprintf(stderr, "Failed to convert %s to long long, error: %s\n", buff, strerror(errno));
+                        return false;
+                    }
+                    return true;
+                }
+                else
+                {
+                    // solo podríamos estar aquí si leamos
+                    // un '-' y después nada, eso es un error
+                    fprintf(stderr, "Found invalid entry \"-\"\n");
+                    return false;
+                }
+            }
+            else if (c == '-')
+            {
+                // un - aquí no es válido porque estámos en el medio
+                // de un int.
+                fprintf(stderr, "Found \"-\" in the middle of an integer\n");
+                return false;
+            }
+            else if (c >= '0' && c <= '9')
+            {
+                // válido
+                if (idx >= MAX_CHARS)
+                {
+                    fprintf(stderr, "Integer read was too large to fit into a long long\n");
+                    return false;
+                }
+                buff[idx++] = c;
+                *OK = true;
+            }
+            else
+            {
+                // error
+                fprintf(stderr, "Found invalid character %c\n", c);
+                return false;
+            }
+        }
+    }
 }
 
-long long *leerEntrada(const char *archivo, unsigned int *filas, unsigned int *columnas)
+bool leerLinea(FILE *f, long long *data, uint columnasEsperados, bool *eof)
+{
+    uint32_t count = 0;
+    while (1)
+    {
+        bool OK;
+        bool newLine;
+        long long ll;
+        if (!leerLongLong(f, &ll, &OK, eof, &newLine))
+        {
+            // error
+            return false;
+        }
+
+        if (OK)
+        {
+            // leamos un integer
+            if (count >= columnasEsperados)
+            {
+                // error - quisimos columnasEsperados integers
+                // pero ya encontramos más.
+                fprintf(stderr, "Too many entries on line. Expecting %u\n", columnasEsperados);
+                return false;
+            }
+
+            data[count] = ll;
+            count++;
+        }
+
+        if (*eof)
+        {
+            if (count != columnasEsperados)
+            {
+                // error - quisimos columnasEsperados integers
+                // pero solo leamos count antes de eof
+                fprintf(stderr, "Not enough entries on line. Expecting %u, found %u\n", columnasEsperados, count);
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        if (newLine)
+        {
+            if (count == 0) // permitimos newLines antes de data comenzando
+            {
+                continue;
+            }
+            else if (count != columnasEsperados)
+            {
+                // error - quisimos columnasEsperados integers
+                // pero solo leamos count antes de newLine.
+                fprintf(stderr, "Not enough entries on line. Expecting %u, found %u\n", columnasEsperados, count);
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+    return true;
+}
+
+long long *leerEntrada(const char *archivo, uint *filas, uint *columnas)
 {
     FILE *f = fopen(archivo, "r");
     if (f == NULL)
@@ -59,7 +270,8 @@ long long *leerEntrada(const char *archivo, unsigned int *filas, unsigned int *c
 
     long long primerLinea[2];
 
-    if (!leerLinea(primerLinea, 2))
+    bool eof;
+    if (!leerLinea(f, primerLinea, 2, &eof))
     {
         fclose(f);
         return NULL;
@@ -80,8 +292,8 @@ long long *leerEntrada(const char *archivo, unsigned int *filas, unsigned int *c
         return NULL;
     }
 
-    *filas = *(unsigned int *)llFilas;
-    *columnas = *(unsigned int *)llColumnas;
+    *filas = *(uint *)llFilas;
+    *columnas = *(uint *)llColumnas;
 
     // numero de elementos = filas * columnas
     // cada uno es un long long, así:
@@ -93,33 +305,70 @@ long long *leerEntrada(const char *archivo, unsigned int *filas, unsigned int *c
         return NULL;
     }
 
-    unsigned int i;
+    uint i;
     for (i = 0; i < *filas; i++)
-    if (!leerLinea(&entrada[i * *columnas], *columnas))
     {
-        fclose(f);
-        free(entrada);
-        return NULL;
+        if (!leerLinea(f, &entrada[i * *columnas], *columnas, &eof))
+        {
+            fclose(f);
+            free(entrada);
+            return NULL;
+        }
+    }
+
+    // debería estar todo, comprobar que no hay más data
+    while (!eof)
+    {
+        if (!leerLinea(f, NULL, 0, &eof))
+        {
+            fclose(f);
+            free(entrada);
+            return NULL;
+        }
     }
 
     fclose(f);
     return entrada;
 }
 
-int escribirSalida(const char *archivo, long long *salida)
+bool escribirSalida(const char *archivo, uint filas, uint columnas, long long *salida)
 {
-#warning TODO
+    FILE *f;
 
     if (archivo == NULL)
     {
         // stdout
+        f = stdout;
     }
     else
     {
         // archivo
+        f = fopen(archivo, "w");
+        if (f == NULL)
+        {
+            fprintf(stderr, "Failed to open %s for writing\n", archivo);
+            return NULL;
+        }
     }
 
-    return 0;
+    fprintf(f, "%u %u\n", filas, columnas);
+    uint i;
+    for (i = 0; i < filas; i++)
+    {
+        uint c;
+        for (c = 0; c < columnas; c++)
+        {
+            fprintf(f, "%lld ", salida[(i * columnas) + c]);
+        }
+        fprintf(f, "\n");
+    }
+
+    if (archivo != NULL)
+    {
+        fclose(f);
+    }
+
+    return true;
 }
 
 int main(int argc, char **argv)
@@ -213,8 +462,8 @@ int main(int argc, char **argv)
     }
 
     // leer archivo
-    unsigned int filas;
-    unsigned int columnas;
+    uint filas;
+    uint columnas;
     long long *entrada = leerEntrada(argv[optind], &filas, &columnas);
     if (entrada == NULL)
     {
@@ -240,7 +489,9 @@ int main(int argc, char **argv)
         return 1;
     }
 
-    int res = escribirSalida(oArchivo, salida);
+    // escribir el resultado
+    // nota que intercambiamos columnas y filas
+    bool res = escribirSalida(oArchivo, columnas, filas, salida);
     free(entrada);
     free(salida);
 
