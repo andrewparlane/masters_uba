ab5080b223b6103c479296e1452f366ccd063681
Andrew Parlane <andrew.parlane@gmail.com>
2018-04-22 17:14:36 -0300

    TP1: Anadí más pruebas y una regla a ejectuarles.

---
+++ tp1/pruebaScript.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Argumentos:
+#  $1 applicación
+#  $2 entrada en pruebas/ sin la ruta
+#  $3 código de salida esperada
+
+./$1 -o salida pruebas/$2 > stdout 2> stderr
+export RES_CODE=$?
+if [ $RES_CODE -eq $3 ]; then
+    if [ $RES_CODE -ne 0 ]; then
+        echo "  OK";
+    else
+        diff -Z salida pruebas/esperados/$2 > /dev/null;
+        if [ $? -eq 0 ]; then
+            echo "  OK";
+        else
+            echo "  Transpuesta no es como esperada";
+        fi
+    fi
+else
+    echo "  Código de salida no es esperado";
+fi
