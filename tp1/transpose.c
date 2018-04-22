b7180a8358e3f4ef1c3d0c5d43afa3968ce26bdb
Andrew Parlane <andrew.parlane@gmail.com>
2018-04-22 15:34:52 -0300

    TP1: Ímplementé transponer en C

--- tp1/transpose.c
+++ tp1/transpose.c
@@ -1,9 +1,17 @@
 #include <stdio.h>
 #include <stdint.h>
-#include <stdbool.h>
 
 int transponer(unsigned int filas, unsigned int columnas, long long *entrada, long long *salida)
 {
-#warning TODO
+    uint f;
+    for (f = 0; f < filas; f++)
+    {
+        uint c;
+        for (c = 0; c < columnas; c++)
+        {
+            salida[(c * filas) + f] = entrada[(f * columnas) + c];
+        }
+    }
+
     return 0;
 }
