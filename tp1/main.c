fbacac4c213fbfe1a7ff5216e930ddf68f6789c7
Andrew Parlane <andrew.parlane@gmail.com>
2018-04-26 14:17:46 -0300

    TP1: v1.0

--- tp1/main.c
+++ tp1/main.c
@@ -8,8 +8,8 @@
 #include <stdbool.h>
 #include <errno.h>
 
-#define MAJOR_VERSION   0
-#define MINOR_VERSION   1
+#define MAJOR_VERSION   1
+#define MINOR_VERSION   0
 
 
 #   +---------------+   |------------MAIN------------
