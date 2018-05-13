54e4061c047d5ad5d866eb64c4aa1af3a222a9e6
Andrew Parlane <andrew.parlane@gmail.com>
2018-05-13 16:28:53 -0300

    Common: Renamed utils to utils_pkg

--- common/src/utils_pkg.vhd
+++ common/src/utils_pkg.vhd
@@ -2,13 +2,13 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.math_real.all;
 
-package utils is
+package utils_pkg is
     function min_width(max_value: natural) return natural;
-end package utils;
+end package utils_pkg;
 
-package body utils is
+package body utils_pkg is
     function min_width(max_value: natural) return natural is
     begin
         return integer(ceil(log2(real(max_value))));
     end function min_width;
-end package body utils;
+end package body utils_pkg;
