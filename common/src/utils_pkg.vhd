7cc866341169e1f7143be3eae424c00141dfa97d
Andrew Parlane <andrew.parlane@gmail.com>
2018-05-27 16:02:39 -0300

    Added a procedure to the utils package that lets you read an unsigned from a line.
    This is useful because you can't read decimals > signed 32 bit max using
    the default functions.

--- common/src/utils_pkg.vhd
+++ common/src/utils_pkg.vhd
@@ -1,10 +1,14 @@
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.math_real.all;
+use ieee.numeric_std.all;
+use std.textio.all;
 
 package utils_pkg is
     function min_width(max_value: natural) return natural;
     function vector_to_string(vect: std_ulogic_vector) return string;
+    procedure read_unsigned_decimal_from_line(l: inout line;
+                                              u: inout unsigned);
 end package utils_pkg;
 
 package body utils_pkg is
@@ -21,4 +25,29 @@ package body utils_pkg is
         end loop;
         return str;
     end function vector_to_string;
+
+    procedure read_unsigned_decimal_from_line(l: inout line;
+                                              u: inout unsigned) is
+        variable c:         character;
+        variable notEnd:    boolean := true;
+        variable i:         integer;
+    begin
+        u := (u'range => '0');
+
+        while notEnd loop
+            read(l, c, notEnd);
+            if (notEnd) then
+                -- convert c to an integer
+                i := character'pos(c) - character'pos('0');
+                if ((i < 0) or (i > 9)) then
+                    -- out of range
+                    notEnd := false;
+                else
+                    u := resize((u * to_unsigned(10, u'length)),
+                                u'length)
+                         + to_unsigned(i, u'length);
+                end if;
+            end if;
+        end loop;
+    end procedure read_unsigned_decimal_from_line;
 end package body utils_pkg;
