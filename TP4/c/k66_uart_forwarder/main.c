329c1e8d1d4ef3528533df895ed0630c112f5fb4
Andrew Parlane <andrew.parlane@gmail.com>
2018-07-16 12:53:29 -0300

    TP4: UART FORWARDER: Finished off the code and tested it works.

--- TP4/c/k66_uart_forwarder/main.c
+++ TP4/c/k66_uart_forwarder/main.c
@@ -1,20 +1,41 @@
 #include "pin_mux.h"
 #include "clock_config.h"
-#include "fsl_debug_console.h"
+#include "fsl_uart.h"
 
 #include <stdio.h>
 
-#define MSG DbgConsole_Printf
+const char *WELCOME_MSG = "K66 UART Forwarder\n" \
+                          "Waiting for data\n";
 
 void main(void)
 {
     BOARD_InitBootPins();
     BOARD_InitBootClocks();
-    DbgConsole_Init(((uint32_t)(UART0)),
-                    115200,
-                    (DEBUG_CONSOLE_DEVICE_TYPE_UART),
-                    CLOCK_GetCoreSysClkFreq());
 
-    MSG("Hello world\n");
-    while (1);
+    // set up the debug uart channel
+    uart_config_t config;
+    UART_GetDefaultConfig(&config);
+    config.enableTx = true;
+    config.enableRx = true;
+    status_t res = UART_Init(UART0, &config, CLOCK_GetCoreSysClkFreq());
+    if (res != kStatus_Success)
+    {
+        while (1);
+    }
+
+    res = UART_Init(UART1, &config, CLOCK_GetCoreSysClkFreq());
+    if (res != kStatus_Success)
+    {
+        while (1);
+    }
+
+    UART_WriteBlocking(UART0, WELCOME_MSG, strlen(WELCOME_MSG));
+
+    while (1)
+    {
+        // read char and then send it over UART1
+        uint8_t c;
+        res = UART_ReadBlocking(UART0, &c, 1);
+        UART_WriteBlocking(UART1, &c, 1);
+    }
 }
