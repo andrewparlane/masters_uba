a55be81155788b8c981f9b51d2de448f130baaee
Andrew Parlane <andrew.parlane@gmail.com>
2018-07-15 23:39:03 -0300

    TP4 SRAM_TEST: Updated the sram synth test.
    Now it uses the 100MHz clock,
    has input pin syncronization,
    tracks read counts,
    only reads on change of address or after writing.
    
    Basically it's a lot more comprehensive.

--- TP4/src/synth_test/sram_test.vhd
+++ TP4/src/synth_test/sram_test.vhd
@@ -6,9 +6,12 @@ library common;
 use common.all;
 use common.type_pkg.all;
 
+library altera_mf;
+use altera_mf.all;
+
 entity sram_test is
     port (CLOCK_50:     in      std_ulogic;
-          KEY:          in      std_ulogic_vector(2 downto 0);
+          KEY:          in      std_ulogic_vector(3 downto 0);
           SW:           in      std_ulogic_vector(17 downto 0);
           SRAM_ADDR:    out     std_ulogic_vector(17 downto 0);
           SRAM_DQ:      inout   std_ulogic_vector(15 downto 0);
@@ -34,25 +37,26 @@ architecture synth of sram_test is
     --  1 to get it from the SRAM
     --  2 in the syncronizer
     component sram is
-        port (i_clk:    in      std_ulogic; -- max clk 100MHz
-              i_reset:  in      std_ulogic;
+        port (i_clk:            in      std_ulogic; -- max clk 100MHz
+              i_reset:          in      std_ulogic;
               -- inputs
-              i_addr:   in      unsigned(17 downto 0);
-              i_wdata:  in      std_ulogic_vector(15 downto 0);
-              i_rnw:    in      std_ulogic;
-              i_start:  in      std_ulogic;
+              i_addr:           in      unsigned(17 downto 0);
+              i_wdata:          in      std_ulogic_vector(15 downto 0);
+              i_rnw:            in      std_ulogic;
+              i_start:          in      std_ulogic;
               -- outputs
-              o_rdata:  out     std_ulogic_vector(15 downto 0);
+              o_rdata:          out     std_ulogic_vector(15 downto 0);
               -- status
-              o_busy:   out     std_ulogic;
+              o_busy:           out     std_ulogic;
+              o_rdata_valid:    out     std_ulogic;
               -- bus ports
-              io_data:  inout   std_ulogic_vector(15 downto 0);
-              o_addr:   out     std_ulogic_vector(17 downto 0);
-              o_nCE:    out     std_ulogic;
-              o_nOE:    out     std_ulogic;
-              o_nWE:    out     std_ulogic;
-              o_nLB:    out     std_ulogic;
-              o_nUB:    out     std_ulogic);
+              io_data:          inout   std_ulogic_vector(15 downto 0);
+              o_addr:           out     std_ulogic_vector(17 downto 0);
+              o_nCE:            out     std_ulogic;
+              o_nOE:            out     std_ulogic;
+              o_nWE:            out     std_ulogic;
+              o_nLB:            out     std_ulogic;
+              o_nUB:            out     std_ulogic);
     end component sram;
 
     component multi_seven_seg_hex is
@@ -64,6 +68,23 @@ architecture synth of sram_test is
               display:  out slvArray((CIFRAS - 1) downto 0)(6 downto 0));
     end component multi_seven_seg_hex;
 
+    component altera_std_synchronizer_bundle is
+        generic (DEPTH : integer := 3;              -- must be >= 2
+                 WIDTH : integer := 1);
+        port (clk     : in  std_logic;
+              reset_n : in  std_logic;
+              din     : in  std_logic_vector(width-1 downto 0);
+              dout    : out std_logic_vector(width-1 downto 0));
+    end component altera_std_synchronizer_bundle;
+
+    component pll
+        port (areset:   in  std_logic;
+              inclk0:   in  std_logic;
+              c0:       out std_logic;
+              c1:       out std_logic;
+              locked:   out std_logic);
+    end component;
+
     signal rdata:   unsigned(15 downto 0);
     signal wdata:   unsigned(15 downto 0);
     signal start:   std_ulogic;
@@ -74,11 +95,46 @@ architecture synth of sram_test is
     signal clear:               std_ulogic;
     signal inc:                 std_ulogic;
     signal waitingForRelease:   boolean;
+
+    signal addr:                unsigned(17 downto 0);
+    signal lastAddr:            unsigned(17 downto 0);
+
+    signal readWhenFree:    std_ulogic;
+    signal valid:           std_ulogic;
+    signal v:               unsigned(15 downto 0);
+    signal reads:           unsigned(3 downto 0);
+
+    signal clk100M:         std_ulogic;
+    signal pll_locked:      std_ulogic;
+
 begin
 
-    rst     <= not KEY(0);
-    clear   <= not KEY(1);
-    inc     <= not KEY(2);
+    pll_inst: pll
+        port map (areset  => '0',
+                  inclk0  => CLOCK_50,
+                  c0      => clk100M,
+                  c1      => open,
+                  locked  => pll_locked);
+
+    rst <= not (KEY(0) and pll_locked);
+
+    syncSW: altera_std_synchronizer_bundle
+    generic map (DEPTH => 2,
+                 WIDTH => 18)
+    port map    (clk        => clk100M,
+                 reset_n    => not rst,
+                 din        => std_logic_vector(SW),
+                 unsigned(dout)       => addr);
+
+    syncButtons: altera_std_synchronizer_bundle
+    generic map (DEPTH => 2,
+                 WIDTH => 2)
+    port map    (clk        => clk100M,
+                 reset_n    => not rst,
+                 din(0)     => not KEY(1),
+                 din(1)     => not KEY(3),
+                 dout(0)    => clear,
+                 dout(1)    => inc);
 
     LEDR(0) <= rst;
     LEDR(1) <= clear;
@@ -88,18 +144,36 @@ begin
     LEDR(5) <= start;
     LEDR(6) <= busy;
 
-    start <= '1'; -- always read, except when writing
+    process (clk100M, rst)
+    begin
+        if (rst = '1') then
+            v <= (others => '0');
+            reads <= (others => '0');
+        elsif (rising_edge(clk100M)) then
+            if (valid = '1') then
+                v <= rdata;
+                reads <= reads + to_unsigned(1, 4);
+            end if;
+        end if;
+    end process;
 
-    process (CLOCK_50, rst)
+    process (clk100M, rst)
     begin
         if (rst = '1') then
             rnw <= '1';
             wdata <= (others => '0');
-        elsif (rising_edge(CLOCK_50)) then
+            readWhenFree <= '1';
+            waitingForRelease <= false;
+        elsif (rising_edge(clk100M)) then
+            -- start and rnw only high for one pulse at a time
+            start <= '0';
+            rnw <= '1'; -- read
+
+            lastAddr <= addr;
+
             if (waitingForRelease) then
-                rnw <= '1'; -- read
-                -- do nothing until clear and inc
-                -- are not pressed
+                -- don't allow anything more until
+                -- inc and clear are released
                 if ((inc = '0') and
                     (clear = '0')) then
                     waitingForRelease <= false;
@@ -107,21 +181,31 @@ begin
             elsif (clear = '1') then
                 wdata <= (others => '0');
                 rnw <= '0'; -- write
+                start <= '1';
+                readWhenFree <= '1';
                 waitingForRelease <= true;
             elsif (inc = '1') then
-                wdata <= rdata + to_unsigned(1, wdata'length);
+                wdata <= v + to_unsigned(1, wdata'length);
                 rnw <= '0'; -- write
+                start <= '1';
+                readWhenFree <= '1';
                 waitingForRelease <= true;
             else
                 rnw <= '1'; -- read
+                if ((lastAddr /= addr) or
+                    ((busy = '0') and
+                     (readWhenFree = '1'))) then
+                    start <= '1';
+                    readWhenFree <= '0';
+                end if;
             end if;
         end if;
     end process;
 
-    inst: sram port map (i_clk              => CLOCK_50,
+    inst: sram port map (i_clk              => clk100M,
                          i_reset            => rst,
                          -- input
-                         i_addr             => unsigned(SW),
+                         i_addr             => addr,
                          i_wdata            => std_ulogic_vector(wdata),
                          i_rnw              => rnw,
                          i_start            => start,
@@ -129,6 +213,7 @@ begin
                          unsigned(o_rdata)  => rdata,
                          -- status
                          o_busy             => busy,
+                         o_rdata_valid      => valid,
                          -- bus ports
                          io_data            => SRAM_DQ,
                          o_addr             => SRAM_ADDR,
@@ -140,15 +225,15 @@ begin
 
     multi7SegInst:
     multi_seven_seg_hex generic map (CIFRAS => 8)
-                        port map (clk => CLOCK_50,
+                        port map (clk => clk100M,
                                   rst => rst,
-                                  en => "00001111",
-                                  hex(0) => unsigned(rdata(3 downto 0)),
-                                  hex(1) => unsigned(rdata(7 downto 4)),
-                                  hex(2) => unsigned(rdata(11 downto 8)),
-                                  hex(3) => unsigned(rdata(15 downto 12)),
-                                  hex(7 downto 4) => (to_unsigned(0,4),
-                                                      to_unsigned(0,4),
+                                  en => "10001111",
+                                  hex(0) => unsigned(v(3 downto 0)),
+                                  hex(1) => unsigned(v(7 downto 4)),
+                                  hex(2) => unsigned(v(11 downto 8)),
+                                  hex(3) => unsigned(v(15 downto 12)),
+                                  hex(7) => unsigned(reads(3 downto 0)),
+                                  hex(6 downto 4) => (to_unsigned(0,4),
                                                       to_unsigned(0,4),
                                                       to_unsigned(0,4)),
                                   display(7) => HEX7,
